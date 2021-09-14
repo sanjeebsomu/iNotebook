38fc2b61d0dfe056b23418adaea7413960923ac9
Sanjeeb <sanjeebgochhayat1999@gmail.com>
2021-09-13 23:07:29 -0700

    Storing data into the data base(mongoose model)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,13 @@
-const express = require('express')
+const express = require('express');
+const User = require('../models/User');
 const router = express.Router()
 
-router.get('/', (req, res)=> {
-    obj = {
-        a:'zxvc',
-        num: 123
-    }
-    res.json(obj)
+//Create an User using: POST "/api/auth". Doesn't require authentication.
+router.post('/', (req, res)=> {
+    const user = User(req.body);//data of user according to req.body
+    user.save()
+    console.log(req.body);
+    res.send(req.body)
 })
 
 module.exports = router
